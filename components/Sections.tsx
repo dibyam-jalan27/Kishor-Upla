52d4948986f0f2e094c753ecc31abd89ae720c8d
Dibyam <dibyam1jalan@gmail.com>
2024-09-04 23:51:36 +0530

    Bugs

--- components/Sections.tsx
+++ components/Sections.tsx
@@ -1,6 +1,11 @@
 "use client";
-
+import { Montserrat } from "next/font/google";
 import Image from "next/image";
+
+const monstserret = Montserrat({
+  weight: "700",
+  subsets: ["latin"],
+});
 
 export default function Sections() {
   return (
@@ -19,12 +24,29 @@ export default function Sections() {
             </div>
           </div>
           <div className="md:w-2/5 ml-4">
-            <h1 className="text-3xl md:text-5xl font-bold">
+            <h1
+              className={`${monstserret.style} text-3xl md:text-5xl font-bold`}
+            >
               Kishor Upla, Ph.D.{" "}
             </h1>
-            <h1 className="text-xl md:text-2xl font-medium pt-2">
+            <h1
+              className={`${monstserret.style} text-xl md:text-2xl font-medium pt-3`}
+            >
               Assistant Professor | Electronics Engineering
             </h1>
+            <p
+              className="text-sm md:text-base text-muted-foreground pt-4"
+              style={{ lineHeight: "1.8" }}
+            >
+              I am an academic with a Ph.D. in Electronics and Communication,
+              focusing on multi-resolution image fusion. With over a decade of
+              experience as an Assistant Professor at SVNIT Surat, I've earned
+              recognition for my research in computer vision and image
+              processing. My work has been published in esteemed journals and
+              conferences, and I've been honored with awards like the ERCIM
+              Post-doctoral Fellowship. My research interests include machine
+              learning, deep learning, and biomedical image processing.
+            </p>
           </div>
           <div className="w-1/5"></div>
         </div>
